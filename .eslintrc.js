f3489032b8e8b403b630294c4dc0e33420dd9d9e
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-30 06:21:59 +0100

    implemented eslint to detect standard code violations

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+/**
+ * Eslint is a library that detects code standards violations in JS!
+ * Here are the coding standards to be followed in this project
+ *
+ * Tell Eslint where's code to be checked: "./node_modules/.bin/eslint assets/"
+ */
+module.exports = {
+    extends: ["eslint:recommended"],
+    parserOptions: {
+        ecmaVersion: 6,
+        sourceType: "module",
+        ecmaFeatures: {
+            jsx: true
+        }
+    },
+    env: {
+        browser: true,
+        es6: true,
+        node: true
+    },
+    rules: {
+        "no-console": 0,
+        "no-unused-vars": 0
+    }
+};
