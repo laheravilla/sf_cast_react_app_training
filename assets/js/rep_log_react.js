aafff0ad11fd81d57015833dd1adda88e0d4d49e
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-30 07:25:29 +0100

    React.createElement()

--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,1 +1,17 @@
-console.log("Oh hello World!");
+import React from "react";
+import ReactDom from "react-dom";
+
+const elem = React.createElement(
+    "h2",
+    null,
+    "Hello world!",
+    React.createElement(
+        "span",
+        null,
+        " <3"
+    )
+);
+
+console.log(elem);
+
+ReactDom.render(elem, document.getElementById("lift-stuff-app"));
