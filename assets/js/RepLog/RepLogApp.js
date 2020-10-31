327ff254b9c45a5db8c5f3fb00012f934f645493
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-31 06:47:49 +0100

    collection & rendered a table

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -2,13 +2,54 @@ import React, { Component } from "react";
 
 export default class RepLogApp extends Component {
     render() {
-        let message = "";
-        if (this.props.message) {
-            message = <strong> World</strong>;
+        let title = "";
+        if (this.props.title) {
+            title = <strong>Lift History</strong>;
         }
 
+        const repLogs = [
+            { id: 1, reps: 25, item: "My Laptop", totalWeight: 112.5 },
+            { id: 2, reps: 10, item: "Big fat Cat", totalWeight: 180 },
+            { id: 8, reps: 4, item: "Big fat Cat", totalWeight: 72 }
+        ];
+
+        const repLogElements = repLogs.map(repLog => {
+            return (
+                <tr>
+                    <td>{repLog.itemLabel}</td>
+                    <td>{repLog.reps}</td>
+                    <td>{repLog.totalWeight}</td>
+                    <td>...</td>
+                </tr>
+            );
+        });
+
         return (
-            <h2>Hello {message}!</h2>
+            <div className="col-md-7">
+                <h2>
+                    {title}
+                </h2>
+                <hr/>
+                <table className="table table-striped">
+                    <thead>
+                    <tr>
+                        <th>What</th>
+                        <th>How many times?</th>
+                        <th>Weight</th>
+                        <th>&nbsp;</th>
+                    </tr>
+                    </thead>
+                    <tbody>{repLogElements}</tbody>
+                    <tfoot>
+                        <tr>
+                            <td>&nbsp;</td>
+                            <th>Total</th>
+                            <th>TODO</th>
+                            <td>&nbsp;</td>
+                        </tr>
+                    </tfoot>
+                </table>
+            </div>
         );
     }
 }
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -2,9 +2,9 @@ import React from "react";
 import { render } from "react-dom";
 import RepLogApp from "./RepLog/RepLogApp";
 
-const shouldShowMessage = true;
+const shouldShowTitle = true;
 
 render(
-    <RepLogApp message={shouldShowMessage} />,
+    <RepLogApp title={shouldShowTitle} />,
     document.getElementById("lift-stuff-app")
 );
