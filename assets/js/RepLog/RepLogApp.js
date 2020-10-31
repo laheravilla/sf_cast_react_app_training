cb38bbf70d36ea563675eefc1aaa950f27f5882b
Yurniel LAHERA VILLA <laheravilla@gmail.com>
2020-10-31 06:15:46 +0100

    react components

---
+++ assets/js/RepLog/RepLogApp.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class RepLogApp extends Component {
+    render() {
+        return (
+            <h2>
+                Hello
+                <strong> World</strong>!
+            </h2>
+        );
+    }
+}
--- assets/js/rep_log_react.js
+++ assets/js/rep_log_react.js
@@ -1,26 +1,5 @@
 import React from "react";
-import ReactDom from "react-dom";
+import { render } from "react-dom";
+import RepLogApp from "./RepLog/RepLogApp";
 
-/*
-const elem = React.createElement(
-    "h2",
-    null,
-    "Hello world!",
-    React.createElement(
-        "span",
-        null,
-        " <3"
-    )
-);
- */
-
-const elem = (
-    <h2>
-        Hello
-        <strong> World!</strong>
-    </h2>
-);
-
-console.log(elem);
-
-ReactDom.render(elem, document.getElementById("lift-stuff-app"));
+render(<RepLogApp />, document.getElementById("lift-stuff-app"));
