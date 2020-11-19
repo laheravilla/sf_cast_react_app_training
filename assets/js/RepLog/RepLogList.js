4d95950e494d4e72f4cc613d73a4a8cbba20e9e9
ylah <laheravilla@gmail.com>
2020-11-19 06:46:38 +0100

    smart vs dumb components

--- assets/js/RepLog/RepLogList.js
+++ assets/js/RepLog/RepLogList.js
@@ -1,31 +1,30 @@
-import React, {Component} from "react";
+import React from "react";
 
-export default class RepLogList extends Component {
-    render() {
-        const { highlightedRowId, onRowMouseOver } = this.props;
+// Presentational, stateless component. Receives and print data
+export default function repLogList(props) {
+    const { highlightedRowId, onRowMouseOver } = props;
 
-        const repLogs = [
-            { id: 1, reps: 25, itemLabel: "My Laptop", totalWeight: 112.5 },
-            { id: 2, reps: 10, itemLabel: "Big fat Cat", totalWeight: 180 },
-            { id: 8, reps: 4, itemLabel: "Big fat Cat", totalWeight: 72 }
-        ];
+    const repLogs = [
+        { id: 1, reps: 25, itemLabel: "My Laptop", totalWeight: 112.5 },
+        { id: 2, reps: 10, itemLabel: "Big fat Cat", totalWeight: 180 },
+        { id: 8, reps: 4, itemLabel: "Big fat Cat", totalWeight: 72 }
+    ];
 
-        return (
-            <tbody>
-            {repLogs.map(repLog => (
-                // Each outer element must have a unique key
-                <tr
-                    key={repLog.id}
-                    className={highlightedRowId === repLog.id ? "info" : ""}
-                    onMouseOver={() => onRowMouseOver(repLog.id)}
-                >
-                    <td>{repLog.itemLabel}</td>
-                    <td>{repLog.reps}</td>
-                    <td>{repLog.totalWeight}</td>
-                    <td>...</td>
-                </tr>
-            ))}
-            </tbody>
-        );
-    }
+    return (
+        <tbody>
+        {repLogs.map(repLog => (
+            // Each outer element must have a unique key
+            <tr
+                key={repLog.id}
+                className={highlightedRowId === repLog.id ? "info" : ""}
+                onMouseOver={() => onRowMouseOver(repLog.id)}
+            >
+                <td>{repLog.itemLabel}</td>
+                <td>{repLog.reps}</td>
+                <td>{repLog.totalWeight}</td>
+                <td>...</td>
+            </tr>
+        ))}
+        </tbody>
+    );
 }
