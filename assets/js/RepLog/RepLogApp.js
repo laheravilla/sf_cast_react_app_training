a05d790f6ef54409cc96a0512102f0f8150b9d3c
ylah <laheravilla@gmail.com>
2020-11-20 21:08:55 +0100

    used refs to get elements

--- assets/js/RepLog/RepLogApp.js
+++ assets/js/RepLog/RepLogApp.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import RepLogs from "./RepLogs";
 import PropTypes from "prop-types";
+import { v4 as uuidv4 } from "uuid";
 
 export default class RepLogApp extends Component {
     constructor(props) {
@@ -9,22 +10,31 @@ export default class RepLogApp extends Component {
         this.state = {
             highlightedRowId: null,
             repLogs: [
-                { id: 1, reps: 25, itemLabel: "My Laptop", totalWeight: 112.5 },
-                { id: 2, reps: 10, itemLabel: "Big fat Cat", totalWeight: 180 },
-                { id: 8, reps: 4, itemLabel: "Big fat Cat", totalWeight: 72 }
+                { id: uuidv4(), reps: 25, itemLabel: "My Laptop", totalWeight: 112.5 },
+                { id: uuidv4(), reps: 10, itemLabel: "Big fat Cat", totalWeight: 180 },
+                { id: uuidv4(), reps: 4, itemLabel: "Big fat Cat", totalWeight: 72 }
             ]
         }
         // Whoever calls this method will always refer to this instance
         this.handleRowMouseOver = this.handleRowMouseOver.bind(this);
+        this.handleNewItemSubmit = this.handleNewItemSubmit.bind(this);
     }
 
     handleRowMouseOver(repLogId) {
         this.setState({highlightedRowId: repLogId});
     }
 
-    handleNewItemSubmit(itemName, reps) {
-        console.log("TODO - handle this data");
-        console.log(itemName, reps);
+    handleNewItemSubmit(itemLabel, reps) {
+        const repLogs = this.state.repLogs;
+        const newRep = {
+            id: uuidv4(),
+            itemLabel,
+            reps,
+            totalWeight: Math.floor(Math.random() * 50)
+        };
+
+        repLogs.push(newRep);
+        this.setState({repLogs});
     }
 
     render() {
--- assets/js/RepLog/RepLogCreator.js
+++ assets/js/RepLog/RepLogCreator.js
@@ -1,18 +1,28 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import repLogs from "./RepLogs";
 
 export default class RepLogCreator extends Component {
     constructor(props) {
         super(props);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.quantityInput = React.createRef();
+        this.itemSelect = React.createRef();
     }
 
     handleFormSubmit(Event) {
         Event.preventDefault();
 
         const { onNewItemSubmit } = this.props;
-        onNewItemSubmit('Big Fat Cat', Event.target.elements.namedItem("reps").value);
+        const quantityInput = this.quantityInput.current;
+        const itemSelect = this.itemSelect.current;
+
+        onNewItemSubmit(
+            itemSelect.options[itemSelect.selectedIndex].value,
+            quantityInput.value
+        );
+
+        quantityInput.value = "";
+        itemSelect.selectedIndex = 0;
     }
 
     render () {
@@ -23,7 +33,7 @@ export default class RepLogCreator extends Component {
                         What did you lift?
                     </label>
                     <select id="rep_log_item"
-                            name="item"
+                            ref={this.itemSelect} // Replace [name="item"] with ref to get the element
                             required="required"
                             className="form-control"
                             defaultValue="fat_cat" // Set default value here instead of in option
@@ -41,7 +51,8 @@ export default class RepLogCreator extends Component {
                         How many times?
                     </label>
                     <input type="number" id="rep_log_reps"
-                           name="reps" required="required"
+                           ref={this.quantityInput}
+                           required="required"
                            placeholder="How many times?"
                            className="form-control"/>
                 </div>
